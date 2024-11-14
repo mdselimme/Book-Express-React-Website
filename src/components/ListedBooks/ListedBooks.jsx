2c9173497e018948a37fbd98e8f47aa187ad065d
mdselimme <mdselimdev@gmail.com>
2024-11-14 10:42:14 +0600

    add-tab-edit

--- src/components/ListedBooks/ListedBooks.jsx
+++ src/components/ListedBooks/ListedBooks.jsx
@@ -4,12 +4,12 @@ import WhishlistBooks from "./WhishlistBooks/WhishlistBooks";
 const ListedBooks = () => {
   return (
     <div className="container mx-auto py-10">
-      <div role="tablist" className="tabs tabs-lifted">
+      <div role="tablist" className="tabs tabs-lifted p-4">
         <input
           type="radio"
           name="my_tabs_2"
           role="tab"
-          className="tab"
+          className="tab [--tab-bg:#EC669F] active:text-white font-medium text-[#010001] [--tab-padding:4rem] text-xl h-16"
           aria-label="Reading Books"
           defaultChecked
         />
@@ -24,7 +24,7 @@ const ListedBooks = () => {
           type="radio"
           name="my_tabs_2"
           role="tab"
-          className="tab"
+          className="tab [--tab-bg:#EC669F] active:text-white font-medium text-[#010001] [--tab-padding:4rem] text-xl h-16"
           aria-label="Whislist Books"
         />
         <div
--- src/components/ListedBooks/ReadingBooks/ReadingBook/ReadingBook.jsx
+++ src/components/ListedBooks/ReadingBooks/ReadingBook/ReadingBook.jsx
@@ -1,16 +1,19 @@
 import PropTypes from "prop-types";
 
 const ReadingBook = ({ readbook }) => {
-  const { image } = readbook;
+  const { image, bookName, author } = readbook;
 
   return (
     <div className="my-14">
-      <div className="card lg:card-side bg-base-100 shadow-2xl">
-        <figure>
+      <div className="card lg:card-side bg-base-100 shadow-2xl border p-7">
+        <figure className="w-1/3">
           <img src={image} alt="Album" className="w-80 h-80" />
         </figure>
         <div className="card-body">
-          <h2 className="card-title">New album is released!</h2>
+          <h2 className="text-3xl font-bold my-2 text-[#010001]">{bookName}</h2>
+          <p className="text-xl font-medium text-[#919095] my-3">
+            By : {author}
+          </p>
           <p>Click the button to listen on Spotiwhy app.</p>
           <div className="card-actions justify-end">
             <button className="btn btn-primary">Listen</button>
